9d6c71ffcb89f0caf67a129c12d0d6189df760d6
ntduongggg <thaiduongbt123@gmail.com>
2025-10-22 19:46:42 +0700

    add cname host

--- webapp/settings.py
+++ webapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-otdstore-env.eba-pr32qpxc.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
